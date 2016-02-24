806f14f6e387d82c4993eca9a6403d557161948f
Alan Taylor <alancarltaylor@gmail.com>
2016-02-24 13:32:08 -0700

    initial commit

---
+++ spec/rides_spec.js
@@ -0,0 +1,50 @@
+var rides = require('../lib/rides')
+
+describe('Rides', function(){
+it ('can determine if a child is tall enough to ride a ride', function(){
+  var child1 = {
+  height: 3,
+  name: "Julian",
+  age: 3
+};
+var child2 = {
+  height: 4,
+  name: "Cero",
+  age: 4
+};
+var ride1 = {
+  minHeight: 3,
+  maxHeight: 7
+};
+var ride2 = {
+  minHeight: 4,
+  maxHeight: 7
+};
+  expect(rides.isTallEnough(child1, ride2)).toEqual(false)
+  expect(rides.isTallEnough(child1, ride1)).toEqual(true)
+});
+it ('can determine if a child is too short', function(){
+  var child1 = {
+  height: 3,
+  name: "Julian",
+  age: 3
+};
+var child2 = {
+  height: 4,
+  name: "Cero",
+  age: 4
+};
+var ride1 = {
+  minHeight: 3,
+  maxHeight: 7
+};
+var ride2 = {
+  minHeight: 4,
+  maxHeight: 7
+};
+  expect(rides.isTallEnough(child2, ride2)).toEqual(true)
+  expect(rides.isTallEnough(child2, ride1)).toEqual(true)
+});
+
+
+});
